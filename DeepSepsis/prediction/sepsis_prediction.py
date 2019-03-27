dbdff1055e1c01993e8bdbb774667d24c3c07a04
Srinivasan Sivanandan <srini2351994@gmail.com>
2019-03-26 22:57:42 -0400

    Adding RNN framework for RNN baseline experiments, Adding GRU cell for experimentation, Adding train,test data preparation

---
+++ DeepSepsis/prediction/sepsis_prediction.py
@@ -0,0 +1,93 @@
+"""
+Author: Srinivasan Sivanandan
+"""
+import sys
+sys.path.append("..")
+import argparse
+import os
+import tensorflow as tf
+from datautils import readData
+import GRUD
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Arguments for sepsis prediction')
+    parser.add_argument('--batch-size', type=int, default=100)
+    parser.add_argument('--data-path', type=str, default="../../data/train")
+    parser.add_argument('--model-path', type=str, default=None)
+    parser.add_argument('--result-path', type=str, default=None)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=1000)
+    parser.add_argument('--n-inputs', type=int, default=36)
+    parser.add_argument('--n-hidden-units', type=int, default=49)
+    parser.add_argument('--n-classes', type=int, default=1)
+    parser.add_argument('--checkpoint-dir', type=str, default='checkpoint',
+                        help='Directory name to save the checkpoints')
+    parser.add_argument('--log-dir', type=str, default='logs',
+                        help='Directory name to save training logs')
+    parser.add_argument('--normalize',type=int,default=1)
+    parser.add_argument('--dropout-rate',type=float,default=0.5)
+    parser.add_argument('--celltype', type=str, default='GRUD')
+    parser.add_argument('--experiment', type=str, default='GRUD')
+    parser.add_argument('--threshold', type=float, default=0.5)
+
+
+    args = parser.parse_args()
+    
+    if args.normalize==0:
+            args.normalize=False
+    if args.normalize==1:
+            args.normalize=True
+
+    checkdir=args.checkpoint_dir
+    logdir=args.log_dir
+    base=args.data_path
+    data_paths=[]
+    max_auc = 0.0
+
+    args.checkpoint_dir=os.path.join(checkdir, args.experiment)
+    args.log_dir=os.path.join(logdir,args.experiment)
+    
+    train_data=readData.ReadData(path=args.data_path,
+                                batchSize=args.batch_size, 
+                                isTrain=True, 
+                                normalize=args.normalize, 
+                                padding=True, 
+                                mean = None, 
+                                std = None)
+    
+    test_data=readData.ReadData(path=args.data_path.replace("train","test"),
+                            batchSize=args.batch_size,
+                            isTrain=False,
+                            normalize=args.normalize,
+                            padding=True,
+                            mean=train_data.mean,
+                            std=train_data.std,
+                            maxLength=train_data.maxLength)
+
+        
+    lrs=[0.001]#[0.004,0.003,0.005,0.006,0.007,0.008,0.009,0.01,0.012,0.015]
+    for lr in lrs:
+        args.lr=lr
+        print("epoch: %2d"%(args.epochs))
+        tf.reset_default_graph()
+        config = tf.ConfigProto() 
+        config.gpu_options.allow_growth = True 
+        with tf.Session(config=config) as sess:
+            model = GRUD.grud(sess,
+                        args=args,
+                        train_data=train_data,
+                        test_data=test_data
+                        )
+            # build computational graph
+            model.build()
+
+            # Train model - (Internally validate the model on test set)
+            auc = model.train()
+            if auc > max_auc:
+                max_auc = auc
+            
+    print("max auc is: " + str(max_auc))
+    f2 = open(('_').join(["max_auc",args.celltype]),"w")
+    f2.write(str(max_auc))
+    f2.close()
+
