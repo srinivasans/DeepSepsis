e1f1f4d50cba3ca692c2dddeaaa328fa175f4ca7
Srinivasan Sivanandan <srini2351994@gmail.com>
2019-03-29 03:41:45 -0400

    Updating the default parameters

--- DeepSepsis/prediction/sepsis_prediction.py
+++ DeepSepsis/prediction/sepsis_prediction.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     parser.add_argument('--batch-size', type=int, default=100)
     parser.add_argument('--data-path', type=str, default="../../data/train")
     parser.add_argument('--model-path', type=str, default=None)
-    parser.add_argument('--result-path', type=str, default=None)
+    parser.add_argument('--result-path', type=str, default='results')
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--n-inputs', type=int, default=36)
-    parser.add_argument('--n-hidden-units', type=int, default=100)
+    parser.add_argument('--n-hidden-units', type=int, default=200)
     parser.add_argument('--n-classes', type=int, default=1)
     parser.add_argument('--checkpoint-dir', type=str, default='checkpoint',
                         help='Directory name to save the checkpoints')
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('--normalize',type=int,default=1)
     parser.add_argument('--dropout-rate',type=float,default=0.7)
     parser.add_argument('--celltype', type=str, default='GRUD')
-    parser.add_argument('--experiment', type=str, default='GRUD')
+    parser.add_argument('--experiment', type=str, default='GRUDAct')
     parser.add_argument('--threshold', type=float, default=0.5)
 
     args = parser.parse_args()
