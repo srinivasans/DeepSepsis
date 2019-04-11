ea14c707b9d93fe4c55d0d37341a3b91b2dcd564
Daniel D <cloudcrypt@users.noreply.github.com>
2019-04-10 23:12:46 -0400

    Improve all the Cox

--- Run-All-Cox.sh
+++ Run-All-Cox.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-script cox.out
 
 for imputation in mean forward
 do
     for i in 1 21 23 30
     do
-        python3 Cox.py --imputation_mode $imputation --seed $i --positive_weight 56
+        python3 Cox.py --imputation_mode $imputation --seed $i --positive_weight 56 | tee COX-"$imputation"_seed_"$i".out
     done
 done
-
-exit
